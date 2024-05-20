3834555cc3490cce6a4a47e6a0b3702144762eec
mariashubna <mariashubna@gmail.com>
2024-05-20 15:28:04 +0200

    .

--- task_7.py
+++ task_7.py
@@ -1,22 +1,49 @@
-import pulp
+import random
+import matplotlib.pyplot as plt
 
-# Ініціалізація моделі
-model = pulp.LpProblem("Maximize Profit", pulp.LpMaximize)
+def play_result(value):
+    result = {i: 0 for i in range(2, 13)}
+    
+    for _ in range(value):
+        first = random.randint(1, 6)
+        second = random.randint(1, 6)
+        sum = first + second        
+        result[sum] += 1
+    
+    probabilities = {s: (count / value) * 100 for s, count in result.items()}    
+    return probabilities
 
-# Визначення змінних
-A = pulp.LpVariable('A', lowBound=0, cat='Integer')  # Кількість продукту А
-B = pulp.LpVariable('B', lowBound=0, upBound=10, cat='Integer')  # Кількість продукту Б
+def plot_results(simulated_probabilities, theoretical_probabilities, value):
+    sums = list(simulated_probabilities.keys())
+    simulated_probs = [simulated_probabilities[sum_dice] for sum_dice in sums]
+    theoretical_probs = [theoretical_probabilities[sum_dice] for sum_dice in sums]
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(sums, simulated_probs, label='Simulated Probabilities', marker='o')
+    plt.plot(sums, theoretical_probs, label='Theoretical Probabilities', marker='x')
+    plt.xlabel('Sum of Two Dice')
+    plt.ylabel('Probability (%)')
+    plt.title(f'Simulated vs Theoretical Probabilities of Dice Sums (number of dice tosses {value})')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
-# Функція цілі (Максимізація прибутку)
-model += 50 * A + 40 * B, "Profit"
 
-# Додавання обмежень
-model += 5 * A + 2 * B <= 80  # Обмеження для машини №1
-model += 3 * A + 2 * B <= 40  # Обмеження для машини №2
+theoretical_probabilities = {
+    2: 1/36 * 100,
+    3: 2/36 * 100,
+    4: 3/36 * 100,
+    5: 4/36 * 100,
+    6: 5/36 * 100,
+    7: 6/36 * 100,
+    8: 5/36 * 100,
+    9: 4/36 * 100,
+    10: 3/36 * 100,
+    11: 2/36 * 100,
+    12: 1/36 * 100
+}
 
-# Розв'язання моделі
-model.solve()
-
-# Вивід результатів
-print("Виробляти продуктів А:", A.varValue)
-print("Виробляти продуктів Б:", B.varValue)
+experement_play = [100, 1000, 100000, 500000]
+for exp in experement_play:
+    simulated_probabilities = play_result(exp)
+    plot_results(simulated_probabilities, theoretical_probabilities, exp)
